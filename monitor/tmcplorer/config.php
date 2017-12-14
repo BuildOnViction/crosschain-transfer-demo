ba6edffba27c25062eee296af3dd1fd3a37dcf7a
Nguyen Sy Thanh Son <thanhson1085@gmail.com>
2017-12-14 17:27:39 +0700

    fix bug tmcplorer

--- monitor/tmcplorer/config.php
+++ monitor/tmcplorer/config.php
@@ -4,5 +4,9 @@ return array(
     "server" => "mongodb://mongodb:27017",
     "dbName" => "tomodb",
   ),
+  "client" => array(
+    "ga" => null,
+    "testnet" => null
+  ),
   "ethereum" => "http://tomochain:8545"
 );
